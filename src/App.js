02a3445c18607411b8a3ca6f3dcb514e3537803d
wannabefrontdeveloper <giwonk786064@gmail.com>
2023-12-28 17:48:05 +0900

    NotFound페이지를 라우팅설정하였습니다.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Profile from "./pages/Profile";
 import Article from "./pages/Article";
 import Articles from "./pages/Articles";
 import Layout from "./Layout";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
