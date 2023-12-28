29142de4a48da1bbe64059bca0644a21c353aeaa
wannabefrontdeveloper <giwonk786064@gmail.com>
2023-12-28 17:40:09 +0900

    NavLink를 감싼 또 다른 컴포넌트를 만들어서 리팩터링하였습니다.

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,41 +1,33 @@
 import { NavLink, Outlet } from "react-router-dom";
 
 const Articles = () => {
-  const activeStyle = {
-    color: "green",
-    fontSize: 21,
-  };
   return (
     <div>
       <Outlet />
       <ul>
-        <li>
-          <NavLink
-            to="/articles/1"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            게시글 1
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/articles/2"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            게시글 2
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/articles/3"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            게시글 3
-          </NavLink>
-        </li>
+        <ArticleItem id={1} />
+        <ArticleItem id={2} />
+        <ArticleItem id={3} />
       </ul>
     </div>
   );
 };
 
+const ArticleItem = ({ id }) => {
+  const activeStyle = {
+    color: "green",
+    fontSize: 21,
+  };
+
+  return (
+    <li>
+      <NavLink
+        to={`/articles/${id}`}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+      >
+        게시글 {id}
+      </NavLink>
+    </li>
+  );
+};
 export default Articles;
