1bcf76febe7cadb1483c16408b3b2c5616da66af
wannabefrontdeveloper <giwonk786064@gmail.com>
2023-12-28 16:46:04 +0900

    Profile 페이지로 이동할 수 있도록 하였습니다.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,20 @@ const Home = () => {
     <div>
       <h1>홈</h1>
       <p>가장 먼저 보여지는 페이지입니다.</p>
-      <Link to="/about">소개</Link>
+      <ul>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles/giwonk">giwonk의 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profiles/pinkmustard">pinkmustard의 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profiles/void">존재하지 않는 프로필</Link>
+        </li>
+      </ul>
     </div>
   );
 };
