7f47d30364628e7550ae87af8a1016f6b9761cef
wannabefrontdeveloper <giwonk786064@gmail.com>
2023-12-28 17:36:03 +0900

    NavLink를 사용하여 게시글의 링크의 텍스트를 더 키우고 초록색으로 변하게끔 하였습니다.

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,18 +1,37 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 const Articles = () => {
+  const activeStyle = {
+    color: "green",
+    fontSize: 21,
+  };
   return (
     <div>
       <Outlet />
       <ul>
         <li>
-          <Link to="/articles/1">게시글 1</Link>
+          <NavLink
+            to="/articles/1"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            게시글 1
+          </NavLink>
         </li>
         <li>
-          <Link to="/articles/2">게시글 2</Link>
+          <NavLink
+            to="/articles/2"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            게시글 2
+          </NavLink>
         </li>
         <li>
-          <Link to="/articles/3">게시글 3</Link>
+          <NavLink
+            to="/articles/3"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            게시글 3
+          </NavLink>
         </li>
       </ul>
     </div>
