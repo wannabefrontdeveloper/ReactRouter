e901b14d8d299c66e865df593d28c6fce61265b7
wannabefrontdeveloper <giwonk786064@gmail.com>
2023-12-28 16:32:26 +0900

    Home 페이지에서 About 페이지로 이동할 수 있도록 Link 컴포넌트를 사용하였습니다.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div>
       <h1>홈</h1>
       <p>가장 먼저 보여지는 페이지입니다.</p>
+      <Link to="/about">소개</Link>
     </div>
   );
 };
